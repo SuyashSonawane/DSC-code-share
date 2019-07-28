f3f52316bd9d6b513f72cd6a99f9e5a3bf481321
suyashsonawane <suyashsonawane007@gmail.com>
2019-07-28 15:47:02 +0530

    tejas doing tp

--- notice-board-ionic-app/src/app/notices/detailed/detailed.module.ts
+++ notice-board-ionic-app/src/app/notices/detailed/detailed.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
-
+import { PdfViewerModule } from "ng2-pdf-viewer";
 import { IonicModule } from "@ionic/angular";
-
 import { DetailedPage } from "./detailed.page";
 import { Pipe, PipeTransform } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
@@ -29,7 +28,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    PdfViewerModule
   ],
   declarations: [DetailedPage, SafePipe]
 })
