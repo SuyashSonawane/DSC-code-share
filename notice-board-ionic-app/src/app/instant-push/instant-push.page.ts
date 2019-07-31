613858f58af6a098947bda5faee507228bf58a23
suyashsonawane <suyashsonawane007@gmail.com>
2019-07-31 12:13:53 +0530

    Text Only and links

--- notice-board-ionic-app/src/app/instant-push/instant-push.page.ts
+++ notice-board-ionic-app/src/app/instant-push/instant-push.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireFunctions } from "@angular/fire/functions";
+import { ToastController } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-instant-push',
-  templateUrl: './instant-push.page.html',
-  styleUrls: ['./instant-push.page.scss'],
+  selector: "app-instant-push",
+  templateUrl: "./instant-push.page.html",
+  styleUrls: ["./instant-push.page.scss"]
 })
 export class InstantPushPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  title = "";
+  body = "";
+  error: boolean = false;
+  constructor(
+    private func: AngularFireFunctions,
+    public toastController: ToastController,
+    private router: Router
+  ) {}
+  async submit() {
+    if (this.title === "" || this.body === "") this.error = true;
+    else {
+      this.func.httpsCallable("instantpush")({
+        body: this.body,
+        title: this.title
+      });
+      const toast = await this.toastController.create({
+        message: "Push Sent ",
+        duration: 2000
+      });
+      toast.present();
+      this.router.navigate(["/all"]);
+    }
   }
-
+  ngOnInit() {}
 }
