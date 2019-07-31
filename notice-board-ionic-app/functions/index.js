dffe1058e39cd35fae45ff384eeb93e694142582
suyashsonawane <suyashsonawane007@gmail.com>
2019-07-31 08:51:28 +0530

    Edit notifications done

--- notice-board-ionic-app/functions/index.js
+++ notice-board-ionic-app/functions/index.js
@@ -8,7 +8,7 @@ exports.subscribeToTopic = functions.https.onCall(
       return `subscribed to ${data.topic}`;
     }
   );
-  
+  let flag=1;
   exports.unsubscribeFromTopic = functions.https.onCall(
     async (data, context) => {
       await admin.messaging().unsubscribeFromTopic(data.token, data.topic);
@@ -20,6 +20,7 @@ exports.subscribeToTopic = functions.https.onCall(
   exports.sendOnFirestoreCreate = functions.firestore
   .document('notices/{noticeId}')
   .onCreate(async (snapshot,context) => {
+    this.flag=0
     const notice = snapshot.data()
     const noticeId = snapshot.ref.parent.key
     console.log(snapshot.data(), snapshot.ref.parent.key)
@@ -44,3 +45,32 @@ exports.subscribeToTopic = functions.https.onCall(
       
     });
   });
+
+  exports.updateNotice = functions.firestore
+    .document('notices/{noticeId}')
+    .onUpdate((change, context) => {
+      if(change.after.data().update_ts){
+        const newValue = change.after.data();
+        const notification={
+          title:`Change in Notice`,
+          body:`A change has been made in the Notice : ${newValue.title} \nAll are requested to check the following notice to avoid any further confusions\n-${newValue.author}`
+        }
+            newValue.batches.forEach(element => {
+              const payload= {
+                  notification,
+                  webpush: {
+                    notification: {
+                      vibrate: [200, 100, 200]
+                    }
+                  },
+                  topic: element
+                };
+          
+              admin.messaging().send(payload).then(res=>console.log(res));
+              
+            });
+
+      }
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+    });
